e8e41a4f59408613698f4bd1d2b512fbaceaf9cd
Austin <133702741+Austincain1006@users.noreply.github.com>
2025-08-08 14:25:28 -0500

    Fixed invisible text when logging in

--- GamePicker/GamePicker/obj/Debug/net8.0/GamePicker.AssemblyInfo.cs
+++ GamePicker/GamePicker/obj/Debug/net8.0/GamePicker.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GamePicker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fc8cc27e6d11047ab667f0279433ca60798e248")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24d432de8a276ef6b2b7f5c26753d3e1e72e1730")]
 [assembly: System.Reflection.AssemblyProductAttribute("GamePicker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GamePicker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- GamePicker/GamePicker/wwwroot/js/site.js
+++ GamePicker/GamePicker/wwwroot/js/site.js
@@ -2,8 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-document.getElementById('formInput').onfocus = function () {
-    if (this.value === "Default text") { // Check if it's the default
-        this.value = "";
-    }
+
+/*
+document.getElementById('formInput').onkeypress = function () {
+    //this.value = "TEST1";
 };
+
+document.getElementById('formInput').onkeydown = function () {
+    //this.value = "TEST1";
+};
+*/
+
