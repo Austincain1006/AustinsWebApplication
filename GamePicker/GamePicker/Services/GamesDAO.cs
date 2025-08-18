cc272b557ebbceb7bfefe66768192fe197be7747
Austin <133702741+Austincain1006@users.noreply.github.com>
2025-08-17 20:32:44 -0500

    Changed Connection String

--- GamePicker/GamePicker/Services/GamesDAO.cs
+++ GamePicker/GamePicker/Services/GamesDAO.cs
@@ -5,7 +5,7 @@ namespace GamePicker.Services
 {
     public class GamesDAO
     {
-        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=aspnet-GamePicker-b2648162-89e9-46f6-83f0-8c152de87997;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        string connectionString = @"Server=tcp:gamepickerdbserver.database.windows.net;Authentication=Active Directory Default;Database=GamePicker_db;";
         public List<Game> GetAllGames()
         {
             List<Game> foundGames = new List<Game>();
